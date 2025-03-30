ca1a3fd85c4d5a9a894ab93777cf5f8dcc8deb25
Aalron <48339061+Aalron@users.noreply.github.com>
2025-03-30 23:08:10 +0800

    application log query page bug fixed. (#4218)
    * application log query page bug fixed.

--- streampark-console/streampark-console-service/src/main/assembly/script/schema/mysql-schema.sql
+++ streampark-console/streampark-console-service/src/main/assembly/script/schema/mysql-schema.sql
@@ -180,11 +180,12 @@ drop table if exists `t_app_log`;
 create table `t_app_log` (
   `id` bigint not null auto_increment,
   `app_id` bigint default null,
+  `job_type` tinyint default null,
   `cluster_id` varchar(64) collate utf8mb4_general_ci default null,
   `tracking_url` varchar(255) collate utf8mb4_general_ci default null,
   `success` tinyint default null,
   `exception` text collate utf8mb4_general_ci,
-  `option_time` datetime default null,
+  `create_time` datetime default null,
   `option_name` tinyint default null,
   `user_id` bigint default null,
   primary key (`id`) using btree
